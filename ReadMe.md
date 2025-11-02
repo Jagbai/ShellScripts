e79fe29cca2e7a82da72129fa336e6871244ada6
root <root@Agbehemoth.localdomain>
2025-11-02 14:00:49 +0000

    Minor amendments

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+# Basic Performance Stats
+
+This repository contains a simple script that collects basic system performance statistics. The script provides a quick snapshot of CPU usage, memory usage, and disk usage, allowing you to monitor your system’s resources easily.
+
+## Features
+
+- CPU Usage: Shows current CPU utilisation percentage.
+
+- Memory Usage: Displays used and free memory percentages.
+
+- Disk Usage: Reports used and free space for specified drives or filesystems.
--- ServerPerformanceStats.sh
+++ ServerPerformanceStats.sh
@@ -1,8 +1,8 @@
-!/bin/bash
+#!/bin/bash
 
 # CPU Usage
 CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}')
-echo "CPU Usage: $CPU_USAGE%"
+echo "CPU Usage: $CPU_USAGE"
 
 # Memory Usage
 USED_MEM=$(free | awk '/Mem:/ {printf("%.1f", $3/$2 * 100)}')
@@ -15,6 +15,14 @@ echo "Memory Usage used: $USED_MEM%"
 USED_DISK=$(df | awk '/C:\\/ {print $5 "%"}')
 FREE_DISK=$(df | awk '/C:\\/ {print 100%-$5 "%"}')
 
+echo "Disk Usage free: $FREE_DISK%"
+echo "Disk Usage used: $USED_DISK%"
+
 #top 5 processes
-TOP_PROCESSES_CPU=$(ps -aux | sort -k3 | head -n 5)
-TOP_PROCESSES_MEM=$(ps -aux | sort -k4 | head -n 5)
+TOP_PROCESSES_CPU=$(ps -aux --sort=-%cpu | tail -n +2 | head -n 5)
+TOP_PROCESSES_MEM=$(ps -aux --sort=-%mem | tail -n +2 | head -n 5)
+
+echo "Top processes for CPU are"
+echo "$TOP_PROCESSES_CPU"
+echo "Top processes for Memory are"
+echo "$TOP_PROCESSES_MEM"
